ed97c7c37aa5d1fe6ceddd865366e2c052ecef6c
Gerhard Haering <gerhard.haering@alexanderthamm.com>
2023-09-11 10:24:08 +0200

    temporary stuff for creating instance

--- 01/README.md
+++ 01/README.md
@@ -6,4 +6,7 @@ Tasks:
 - Run terraform init
 - Which providers are used?
 - Which files are created and what do they mean?
-
+- Create a terraform plan
+- Save a terraform plan
+- Apply the terraform plan
+- Do the same for a destructive plan
---
+++ 0xx_ec2/instance.tf
@@ -0,0 +1,20 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-arm64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "my_instance" {
+  ami           = data.aws_ami.ubuntu.image_id
+  instance_type = "t4g.nano"
+}
