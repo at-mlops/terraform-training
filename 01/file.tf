69f6de6bed63f6609c2c36c162edbe81b9169b31
Nicholas Kappel <nicholas.kappel@alexanderthamm.com>
2024-02-12 15:24:20 +0100

    minor improvements task 1 and task 2

--- 01/file.tf
+++ 01/file.tf
@@ -1,2 +1,6 @@
 # Use the local_file resource for this task:
 # https://registry.terraform.io/providers/hashicorp/local/latest/docs/resources/file
+
+# Provider "local" allows us to use "local_file" resource. You'll learn more about providers later
+provider "local" {
+}
