671d3ed61c73c33335ad1d7fb5b685db3b551cc8
Nico Bach <nico.bach@alexanderthamm.com>
2024-02-20 13:50:08 +0100

    downgrading azurerm provider to 3.89 to avoid skip_provider_registration issues

--- 03/providers.tf
+++ 03/providers.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.89.0"
+    }
+  }
+}
+
 provider "azurerm" {
+  # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  # skip_provider_registration = true 
   features {}
 }
--- 04/providers.tf
+++ 04/providers.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "azurerm" {
   # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
-  # skip_provider_registration = true 
+  # skip_provider_registration = true
   features {}
 }
